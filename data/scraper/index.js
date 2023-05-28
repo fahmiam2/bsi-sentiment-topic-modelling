037dc36eb08aa083f60edd6639add418fba624c0
fahmiam2 <fahmiam271@gmail.com>
2023-05-28 15:59:31 +0700

    Add try-catch block

--- data/scraper/index.js
+++ data/scraper/index.js
@@ -1,2 +1,12 @@
-require('./gplay-scraper.js')
-require('./apstore-scraper.js')
+try {
+    require('./gplay-scraper.js');
+    } catch (error) {
+        console.log('Error in gplay-scraper:', error);
+}
+  
+try {
+    require('./apstore-scraper.js');
+    } catch (error) {
+        console.log('Error in apstore-scraper:', error);
+}
+  
